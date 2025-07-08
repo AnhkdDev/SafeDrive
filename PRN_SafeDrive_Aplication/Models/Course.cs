1f4e98cf872f6f71f3f15fe025e345ca75cdd4c9
kdadeptrai <funnymobile1230@gmail.com>
2025-07-08 10:19:43 +0700

    upgrade feature allow police view list Exam and Create new Exams

--- PRN_SafeDrive_Aplication/Models/Course.cs
+++ PRN_SafeDrive_Aplication/Models/Course.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace PRN_SafeDrive_Aplication.Models;
 
@@ -7,15 +9,17 @@ public partial class Course
 {
     public int CourseId { get; set; }
 
+    [Column(TypeName = "nvarchar(200)")]
     public string CourseName { get; set; } = null!;
 
     public int TeacherId { get; set; }
 
+    [Column(TypeName = "nvarchar(max)")]
     public string ContentCourse {  get; set; } = null!;
 
-    public DateTime? StartDate { get; set; }
+    public DateOnly StartDate { get; set; }
 
-    public DateTime? EndDate { get; set; }
+    public DateOnly EndDate { get; set; }
 
     public virtual ICollection<Exam> Exams { get; set; } = new List<Exam>();
 
